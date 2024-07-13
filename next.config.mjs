54f74ba913c788863cc20bc50420fa109861b241
Keita Ito <eguchi@eguchi.cc>
2024-07-13 11:52:24 +0900

    chore: ssgに変更

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["three"],
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
